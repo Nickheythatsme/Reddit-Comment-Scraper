4af036eaa6418b78ca911518c705daae44e32874
Nicholas Grout <njgrout@gmail.com>
2018-07-20 10:56:21 -0700

    modified:   run.bat

--- run.bat
+++ run.bat
@@ -3,4 +3,8 @@
 SET client_id='DWgIcrR8lKPqrg'
 SET client_secret='hbsFxEyUyuvm01yFzkttQeK990w'
 SET user_agent='pull comments from reddit and analyze them'
-python3 main.py
+if exist python3 (
+    python3 main.py
+)else (
+    python main.py
+)
